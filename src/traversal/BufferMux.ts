46f9d8a1fb811a9f2a9b1fe609d914cb83663faf
nicklassundin <p4cr@hotmail.com>
2025-05-13 17:02:36 +0200

    add datafiles

---
+++ src/traversal/BufferMux.ts
@@ -0,0 +1,175 @@
+
+const WORKGROUPSIZE = 8;
+class QuadTree {
+	nodes: GPUBuffer;
+	values: GPUBuffer;
+	constructor(device: GPUDevice, json: Array){
+		const values = new Float32Array(json.values);
+		this.values = device.createBuffer({
+			size: values.byteLength,
+			usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
+		});
+		device.queue.writeBuffer(this.values, 0, values.buffer);
+
+		const nodes = new Float32Array(json.nodes);
+		this.nodes = device.createBuffer({
+			size: nodes.byteLength,
+			usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
+		});
+		device.queue.writeBuffer(this.nodes, 0, nodes.buffer);
+	};
+	unbind() {
+		this.nodes.destroy();
+		this.values.destroy();
+	}
+}
+
+class BufferMux {
+	// DATA
+	config: {
+		textureSize: number;
+		mipLevel: number;
+	};
+	quadTrees: QuadTree[];
+	
+	// Dataset Traversal
+	travThreadIter: GPUBuffer;
+	traversal: GPUBuffer;
+	features: GPUBuffer[];
+	
+	// Evaluation
+	quadTreeMap: GPUBuffer;
+	evalThreadIter: GPUBuffer;
+	// Result Texture
+	mipTexture: GPUTexture;
+	// Result Buffer
+	result: GPUBuffer; 
+
+	// Render
+	texture: GPUTexture;
+	sampler: GPUSampler;
+	vertices: GPUBuffer;
+	indices: GPUBuffer;
+	uniform: GPUBuffer;
+	state: GPUBuffer;
+		
+	constructor(device: GPUDevice, 
+		    textureSize: number, 
+		    mipLevel: number,
+		   number_threads: number,
+		   uv: number[],
+		   data: array[]) {
+		this.config = {
+			textureSize: textureSize,
+			mipLevel: mipLevel,
+			workgroupSize: WORKGROUPSIZE,
+		};
+		this.features = [];
+		this.quadTrees = [];
+		// Data Initialization and Traversal of data
+		for (let i = 0; i < data.length; i++) {
+			const quadTree = new QuadTree(device, data[i]);
+			this.quadTrees.push(quadTree);
+			const features = device.createBuffer({
+				size: Float32Array.BYTES_PER_ELEMENT*Math.pow(4, mipLevel),
+				usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
+			});
+			this.features.push(features);
+			const travThreadIter = device.createBuffer({
+				size: Float32Array.BYTES_PER_ELEMENT*Math.pow(4, mipLevel),
+				usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
+			})
+		}
+		const traversal_values = new Float32Array([0, 0, uv[0], uv[1], 0, 0, 1, 1]);
+		this.traversal = device.createBuffer({
+			size: traversal_values.byteLength * Math.pow(4, mipLevel),
+			usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
+		})
+		device.queue.writeBuffer(this.traversal, 0, traversal_values.buffer);
+		this.travThreadIter = device.createBuffer({
+			size: 4*16 + number_threads*4,
+			usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
+		})
+
+
+		// Evaluation Initialization
+		this.quadTreeMap = device.createBuffer({
+			size: Math.pow(2, 2*mipLevel)*4*4,
+			usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
+		})
+		this.mipTexture = device.createTexture({
+			size: [textureSize.width, textureSize.height],
+			format: 'rgba8unorm',
+			usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.COPY_SRC | GPUTextureUsage.COPY_DST,
+			mipLevelCount: mipLevel,
+		})
+		this.evalThreadIter = device.createBuffer({
+			size: 4*16 + number_threads*4,
+			usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
+		})
+		this.result = device.createBuffer({
+			size: this.features[0].size*4*4,
+			offset: 0,
+			usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
+	
+		})
+		// Render Initialization
+		this.texture = device.createTexture({
+			size: [textureSize.width, textureSize.height],
+			format: 'rgba8unorm',
+			usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.COPY_DST,
+		});
+		this.sampler = device.createSampler({
+			minFilter: 'nearest',
+			magFilter: 'nearest',
+			mipmapFilter: 'nearest',
+		});
+		const verticeValues = new Float32Array([
+			0, 0, 0, 1, 0, 0, 0, 1,
+			1, 0, 0, 1, 0, 0, 0, 1,
+			0, 1, 0, 1, 0, 0, 0, 1,
+			1, 1, 0, 1, 0, 0, 0, 1,
+		]);
+		this.vertices = device.createBuffer({
+			// size: Math.pow(4, 8),
+			size: verticeValues.byteLength, 
+			usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.VERTEX,
+		});
+		device.queue.writeBuffer(this.vertices, 0, verticeValues.buffer);
+		const indicesValues = new Uint32Array([0, 1, 2, 1, 3, 2]);
+		this.indices = device.createBuffer({
+			size: 6*4*4, 
+			usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.INDEX,
+		});
+		device.queue.writeBuffer(this.indices, 0, indicesValues.buffer);
+		// State Buffer
+		this.state = device.createBuffer({
+			size: 4*WORKGROUPSIZE*WORKGROUPSIZE + 8*8,
+			usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
+		});
+		// TODO check if textureSize works
+		const resolution = new Float32Array([textureSize.width, textureSize.height]);
+		const workgroupSize = new Uint32Array([WORKGROUPSIZE, WORKGROUPSIZE]); 
+		this.uniform = device.createBuffer({
+			size: 4*WORKGROUPSIZE*WORKGROUPSIZE + 8*8,
+			usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+		});
+		device.queue.writeBuffer(this.uniform, 0, resolution.buffer);
+		device.queue.writeBuffer(this.uniform, 4*4, workgroupSize.buffer);
+		this.uniformSize = resolution.byteLength + workgroupSize.byteLength;
+	};
+	unbind() {
+		this.quadTreeMap.destroy();
+		this.mipTexture.destroy();
+		this.evalThreadIter.destroy();
+		this.result.destroy();
+		for (let i = 0; i < this.features.length; i++) {
+			this.features[i].destroy();
+			this.quadTrees[i].nodes.destroy();
+			this.quadTrees[i].values.destroy();
+			this.quadTrees[i].unbind();
+		}
+		this.travThreadIter.destroy();
+	}
+}
+export default BufferMux;
